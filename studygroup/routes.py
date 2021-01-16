4a704b219815623b7224067e9a802dfd1d4e67f7
rjagetia1 <rjagetia@princeton.edu>
2021-01-16 18:41:56 -0500

    routing should be working

--- studygroup/routes.py
+++ studygroup/routes.py
@@ -13,7 +13,7 @@ import studygroup.db as db
 @app.route('/index')
 
 def index():
-    
+
     html = render_template('index.html')
     response = make_response(html)
 
@@ -21,13 +21,64 @@ def index():
 
 #-----------------------------------------------------------------------
 
-@app.route('/main')
+@app.route('/main', methods=["GET", "POST"])
 def main():
+    if request.method == 'POST':
+        if 'username' in request.form:
+            username = request.form['username']
+        else:
+            username = None
+        if "password" in request.form:
+            password = request.form['password']
+        else: 
+            password = None
+        if 'search' in request.form:
+            search = request.form['search']
+        else:
+            search = None
+        if 'title' in request.form:
+            title = request.form['title']
+        else:
+            title = None
+        if 'description' in request.form:
+            description = request.form['description']
+        else:
+            description = None
+        if 'bio' in request.form:
+            bio = request.form['bio']
+        else:
+            bio = None
+        if 'time' in request.form:
+            time = request.form['time']
+        else:
+            time = None
+        if 'date' in request.form:
+            date = request.form['date']
+        else:
+            date = None
+        if 'max_parps' in request.form:
+            max_parps = request.form['max_parps']
+        else:
+            max_parps = None
+        
+        html = render_template('main.html', username=username, password=password)
+        
+        response = make_response(html)
 
-    html = render_template('main.html')
-    response = make_response(html)
-
-    return response
+        return response
+    if request.method == 'GET':
+        username = request.args.get("username")
+        password = request.args.get("password")
+        if username != None:
+            print(username)
+        if password != None:
+            print(password)
+        
+        html = render_template('main.html', username=username, password=password)
+        
+        response = make_response(html)
+        
+        return response
 
 
 #-----------------------------------------------------------------------
