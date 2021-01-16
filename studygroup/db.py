e6943e2b8dae8f1b56cb17399f67bfd4eeb4362b
Oleg Golev <ogolev@princeton.edu>
2021-01-16 15:27:37 -0500

    full structure of the project

---
+++ studygroup/db.py
@@ -0,0 +1,8 @@
+# -------------------------------------------------------------------------------
+# db_frontend.py
+# Description: Back-end API for dynamic front-end development.
+# -------------------------------------------------------------------------------
+
+# from studygroup.models import
+from studygroup import db
+
