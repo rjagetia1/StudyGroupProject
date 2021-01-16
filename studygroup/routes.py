93156adaf4e4239c30d52e66effb8c104e777205
rjagetia1 <rjagetia@princeton.edu>
2021-01-16 15:59:56 -0500

    basic routing complete

--- studygroup/routes.py
+++ studygroup/routes.py
@@ -6,3 +6,36 @@ from flask import render_template, request, make_response, redirect, url_for, se
 
 from studygroup import app, DEBUG
 import studygroup.db as db
+
+#-----------------------------------------------------------------------
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    
+    html = render_template('index.html')
+    response = make_response(html)
+
+    return response
+
+#-----------------------------------------------------------------------
+
+@app.route('/main')
+def main():
+
+    html = render_template('main.html')
+    response = make_response(html)
+
+    return response
+
+
+#-----------------------------------------------------------------------
+
+@app.route('/my_workshops')
+def my_workshops():
+
+    html = render_template('my_workshops.html')
+    response = make_response(html)
+
+    return response
