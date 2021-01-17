04010dd61a1a5336e4a080d5fe33755076163cf2
rjagetia1 <rjagetia@princeton.edu>
2021-01-16 20:10:37 -0500

    continued working on routing

--- studygroup/routes.py
+++ studygroup/routes.py
@@ -7,6 +7,8 @@ from flask import render_template, request, make_response, redirect, url_for, se
 from studygroup import app, DEBUG
 import studygroup.db as db
 
+username = ''
+
 #-----------------------------------------------------------------------
 
 @app.route('/')
@@ -36,30 +38,16 @@ def main():
             search = request.form['search']
         else:
             search = None
+
         if 'title' in request.form:
-            title = request.form['title']
-        else:
-            title = None
-        if 'description' in request.form:
-            description = request.form['description']
-        else:
-            description = None
-        if 'bio' in request.form:
-            bio = request.form['bio']
-        else:
-            bio = None
-        if 'time' in request.form:
-            time = request.form['time']
-        else:
-            time = None
-        if 'date' in request.form:
-            date = request.form['date']
-        else:
-            date = None
-        if 'max_parps' in request.form:
-            max_parps = request.form['max_parps']
-        else:
-            max_parps = None
+            create_session = {}
+            create_session['title'] = request.form['title']
+            create_session['description'] = request.form['description']
+            create_session['bio'] = request.form['bio']
+            create_session['time'] = request.form['time']
+            create_session['date'] = request.form['date']
+            print(create_session['date'])
+            create_session['max_parps'] = request.form['max_parps']
         
         html = render_template('main.html', username=username, password=password)
         
